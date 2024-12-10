3d9e06918162219e29b9f0881b364e4cd8d740f6
Ray C <hacker4street@gmail.com>
2024-12-10 21:55:46 +0100

    added attributes, constructor and Runnable run method to ClientHandler class

--- Game/ClientHandler.java
+++ Game/ClientHandler.java
@@ -1,5 +1,21 @@
 package Game;
 
-public class ClientHandler {
-    
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class ClientHandler implements Runnable {
+    private Socket socket;
+    private HangmanServer server;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket, HangmanServer server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+    }
 }
